f52d98090ca3fa127a6140e9917be58108bde6b2
EDHScode <kcluchey@ugcloud.ca>
2023-05-25 10:07:44 -0400

    Almost Finished JumpPad
    cool lol hi

---
+++ DANI_Movement _Turtrorial/Assets/JumpPad_Boost.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad_Boost : MonoBehaviour
+{
+    public LayerMask WhatIsJumpPadable;
+    public LayerMask Ground;
+    public float JumpBoostMultiplier = 20f;
+    public Rigidbody rb;
+    public float raycastDistance = 1.2f;
+    public bool JumpBoostAir
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, raycastDistance, WhatIsJumpPadable))
+        {
+            rb.AddForce(Vector3.up * JumpBoostMultiplier, ForceMode.Impulse);
+        }
+        else if (!Physics.Raycast(transform.position, Vector3.down, raycastDistance,Ground))
+        {
+            
+        }
+    }
+
+}
